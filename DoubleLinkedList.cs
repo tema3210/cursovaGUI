6f0ecadebbb210c813002f7d309dd80a848478e5
tema3210 <play.aab.25122001@gmail.com>
2021-02-04 02:27:20 +0200

    implement the model

---
+++ DoubleLinkedList.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursovaGUI
+{
+    /// <summary>
+    ///  This is double linked list that is not thread safe
+    /// </summary>
+    /// <typeparam name="T"> Datatype that list holds</typeparam>
+    class DoubleLinkedList<T>
+    {
+        public DoubleLinkedList(T val)
+        {
+            this.Data = val;
+        }
+        public T Data { get; set; }
+        public DoubleLinkedList<T> Next;
+        public DoubleLinkedList<T> Prev;
+        /// <summary>
+        ///  Excludes a node from a list, the list stays consistent 
+        /// </summary>
+        /// <returns> data </returns>
+        public T Consume()
+        {
+            if (this.Next != null) {
+                this.Next.Prev = this.Prev;
+            };
+            if (this.Prev != null)
+            {
+                this.Prev.Next = this.Next;
+            };
+            return this.Data;
+        }
+        /// <summary>
+        /// create a following node
+        /// </summary>
+        /// <param name="val">data</param>
+        public void InsertForward(T val)
+        {
+            var node = new DoubleLinkedList<T>(val);
+            if (this.Next != null)
+            {
+                this.Next.Prev = node;
+                node.Next = this.Next;
+            }
+            this.Next = node;
+            node.Prev = this;
+        }
+        /// <summary>
+        /// create a previous node
+        /// </summary>
+        /// <param name="val">data</param>
+        public void InsertBackward(T val)
+        {
+            var node = new DoubleLinkedList<T>(val);
+            if (this.Prev != null)
+            {
+                this.Prev.Next = node;
+                node.Prev = this.Prev;
+            }
+            this.Prev = node;
+            node.Next = this;
+        }
+    }
+}
---
+++ ListManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursovaGUI
+{
+    class ListManager<T>
+    {
+        ListManager(T[] members)
+        {
+            var head = new DoubleLinkedList<T>(members[0]);
+            this.inner = head;
+            for (int i = 1;i<members.Length;i++)
+            {
+                this.inner.InsertForward(members[i]);
+                this.inner = this.inner.Next;
+            }
+            this.inner.Next = head;
+            head.Prev = this.inner;
+        }
+
+        public IEnumerable<T> Task(int k)
+        {
+            //init iter
+            var curr = this.inner;
+            while (curr.Prev != curr.Next)
+            {
+                //advance iter
+                for (int i = 0; i < k; i++)
+                    curr = curr.Next;
+                //We save next item
+                var t = curr.Next;
+                //then we yield an item, excluding it from a list
+                yield return curr.Consume();
+                // we fix iter: make it point into a sequence
+                curr = t;
+            };
+            //the last thing in a sequence
+            yield return curr.Data;
+
+            yield break;
+        }
+
+        private DoubleLinkedList<T> inner;
+
+    }
+}
